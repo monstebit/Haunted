d204427e86e573adaa6ea90d2c7e816e2759bb5e
monstebit <78085017+monstebit@users.noreply.github.com>
2024-05-09 13:29:50 +0300

    generate PlayerControls class

---
+++ Assets/Scripts/Character/Player/PlayerInputManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace UU
+{
+    public class PlayerInputManager : MonoBehaviour
+    {
+        private PlayerControls _playerControls;
+        private CharacterController _characterController;
+    }
+}
